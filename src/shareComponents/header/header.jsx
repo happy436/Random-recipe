eb1c266f352ee5eb36366c1a519214a518fa6289
happy436 <lelouch436@gmail.com>
2022-02-09 20:56:07 +0200

    header url

--- src/shareComponents/header/header.jsx
+++ src/shareComponents/header/header.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import s from "./header.module.scss"
 import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = ({rootURL}) => {
     return (
         <div className={s.header}>
-            <NavLink className={s.link} to="/" >
+            <NavLink className={s.link} to={`${rootURL}/`} >
                 Random Dish
             </NavLink>
-            <NavLink className={s.link} to="/favourites" >
+            <NavLink className={s.link} to={`${rootURL}/favourites`} >
                 Favourites
             </NavLink>
         </div>
